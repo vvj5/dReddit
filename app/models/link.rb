65400430a06fa7ca41a3d9ee09f34cdd03cb99fc
Will Sherman <vvj5@icloud.com>
2015-08-21 09:49:14 -0500

    login/logout working

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,10 +1,13 @@
 class Link < ActiveRecord::Base
   acts_as_votable
   belongs_to :User
+  validates :user_id, presence: true
+
+  @link = Link.new(:title => 'my link!')
+  @link.save
+
+  @link.liked_by @user
+  @link.votes_for.size # => 1
 end
 
-@link = Link.new(:title => 'my link!')
-@link.save
 
-@link.liked_by @user
-@link.votes_for.size # => 1
